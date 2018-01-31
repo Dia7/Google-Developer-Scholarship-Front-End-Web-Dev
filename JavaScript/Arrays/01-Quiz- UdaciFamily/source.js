66575d4484b74a95b282c2a014acbf1d0e965b0c
Diana <diana.ratcheva@gmail.com>
2018-01-31 19:47:06 +0200

    Fill the array with data

---
+++ JavaScript/Arrays/01-Quiz- UdaciFamily/source.js
@@ -0,0 +1,12 @@
+/*
+ * Programming Quiz: UdaciFamily (6-1)
+ */
+
+// your code goes here
+var udaciFamily = [];
+udaciFamily[0] = "Julia";
+udaciFamily[1] = "James";
+udaciFamily[2] = "Diana";
+
+console.log(udaciFamily);
+
